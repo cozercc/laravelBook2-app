565be7d1602d64e1cb33c18b7e5891d1fd56afed
cozer <cozercn@gmail.com>
2022-06-30 10:41:25 +0900

    第四章结束

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,23 +2,14 @@
 
 <head>
     <title>@yield('title', 'Laravel App') - Laravel 練習チョウムライ</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
 
 <body>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="/">Weibo App</a>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item"><a class="nav-link" href="/help">help</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">login</a></li>
-            </ul>
-        </div>
-    </nav>
-
+    @include('layouts._header')
     <div class="container">
         @yield('content')
+        @include('layouts._footer')
     </div>
 </body>
 
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', 'Create User')
+
+@section('content')
+    <h1>Create User</h1>
+@stop
