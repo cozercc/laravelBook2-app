d1085718dce0c9413d5639e86e4a0b7af329dfb5
張梦磊 <cozercn@gmail.com>
2022-07-19 19:26:53 +0900

    第六章

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,4 +11,9 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
+
+    public function show(User $user){
+        return view('users.show', compact('user'));
+    }
 }
